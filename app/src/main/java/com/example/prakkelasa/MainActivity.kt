c1d0d62b5e7233e3903c28e86ba10f3c960a0e51
jundial-faruq <jundialfaruq6@gmail.com>
2026-09-25 10:02:51 +0700

    Mengganti variabel nama jadi Kelas A

--- app/src/main/java/com/example/prakkelasa/MainActivity.kt
+++ app/src/main/java/com/example/prakkelasa/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PrakKelasATheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Kelas A",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
